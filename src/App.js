88e1bbf0dcbc504562b0d9ed5dfe75fb3b2da594
pritam pandey <pritampandeymail@gmail.com>
2022-05-22 16:28:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "D:/React_Todo/my-todo-list/src/Component/Todo.js";
+
+function App() {
+  return (
+    <div>
+     <Todo/>
+    </div>
+  );
+}
+
+export default App;
